9e13c68eba8744cc39a9e0ff7128a6aa7abbf12f
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2026-01-01 12:06:34 +0000

    Time: 0 ms (100.00%) | Memory: 4.3 MB (20.69%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.go
+++ 8-string-to-integer-atoi/string-to-integer-atoi.go
@@ -9,45 +9,42 @@ import
 func myAtoi(s string) int {
     strar := []rune(s) 
     charindex := 0  
+    //remove white space at the beginning
     for i := 0; i < len(strar); i++{
         if !unicode.IsSpace(strar[i]){
             charindex = i;
             break
-        }  
-        fmt.Println(string(strar))
+        }   
     }
-    strar = strar[charindex:] 
-    fmt.Println(string(strar))
+    strar = strar[charindex:]  
 
-    if len(strar) == 0{
+    //return 0 if blank string or if the first two characters are not digits
+    if (len(strar) == 0) || (len(strar) > 1 && !unicode.IsDigit(strar[0]) && !unicode.IsDigit(strar[1])){
         return 0;
-    } 
+    }  
 
-    if len(strar) > 1 && !unicode.IsDigit(strar[0]) && !unicode.IsDigit(strar[1]){
-        fmt.Println("two non digits")
-        return 0
-    } 
     s = string(strar)
-    var negative bool = false
-    fmt.Println("string after strar" + s)
+
+    //check if integer is positive or negative
+    var negative bool = false 
     if s[0] == '-' {
         s = strings.Replace(s, "-", "", 1)
         negative = true
     } else if s[0] == '+' {
         s = strings.Replace(s, "+", "", 1)
-    }
-    fmt.Println(s)
-    for i, c := range s {
-        fmt.Println(string(c))
-        fmt.Println(unicode.IsDigit(c))
+    } 
+
+    //if any characters are in the string, remove everything after that point
+    for i, c := range s {  
         if !unicode.IsDigit(c){
             s = s[:i]
             break
         }
-    }
-    fmt.Println(s)
+    } 
+    //convert sanitised string to int
     val, _ := strconv.Atoi(s);
     
+    //rounding if number isn't 32 bit
     if negative{
         val = -val
         if val < -2147483648{
