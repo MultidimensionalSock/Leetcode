4c18e35a7985604844c405a4e7dc38b82df7e6e5
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2026-01-01 17:16:45 +0000

    Time: 0 ms (100.00%) | Memory: 4.2 MB (75.99%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.go
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.go
@@ -1,12 +1,8 @@
 func minimumOperations(nums []int) int {
     min := 0
     for i := 0; i < len(nums); i++{
-        mod := nums[i] % 3
-        switch(mod){
-            case 1: 
-                min += 1
-            case 2: 
-                min += 1
+        if nums[i] % 3 != 0{
+            min++
         } 
     }
     return min
