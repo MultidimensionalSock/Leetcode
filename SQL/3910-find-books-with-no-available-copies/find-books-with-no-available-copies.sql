23b67ec982670fdcf63aae0a9c70f11f325b81ff
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2026-01-02 22:04:54 +0000

    Time: 361 ms (88.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -0,0 +1,18 @@
+/* Write your T-SQL query statement below */
+select 
+    l.book_id, 
+    l.title, 
+    l.author, 
+    l.genre, 
+    l.publication_year, 
+    b.currently_borrowed as current_borrowers 
+from library_books as l 
+join (
+    select book_id, count(*) as currently_borrowed 
+    from borrowing_records
+    where return_date is null
+    group by book_id 
+) as b
+on b.book_id = l.book_id
+where l.total_copies = b.currently_borrowed
+order by current_borrowers desc, l.title 
