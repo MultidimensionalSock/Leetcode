00ec892ccdb20000ec697119b054d4aa78238d3d
Erithacus <92083124+MultidimensionalSock@users.noreply.github.com>
2026-01-04 12:25:54 +0000

    Time: 5 ms (6.95%) | Memory: 7 MB (5.11%) - LeetSync

---
+++ Go/2-add-two-numbers/add-two-numbers.go
@@ -0,0 +1,107 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+ import
+ (
+    "strconv"
+    "fmt"
+ )
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    l1str := ReverseString(LinkedListToString(l1)) 
+    //l1int, _ := strconv.Atoi(l1str)
+    fmt.Println(l1str)
+    l2str := ReverseString(LinkedListToString(l2)) 
+    //l2int, _ := strconv.Atoi(l2str)
+    fmt.Println(l2str)
+    
+    //var inttotal uint64 = uint64(l1int) + uint64(l2int)
+    largetotal := addLargeNumbers(l1str, l2str)
+    root := CreateLinkedList(largetotal)
+    return root
+}
+
+func CreateLinkedList(s string) *ListNode{
+    array := []rune(s)  
+    var root *ListNode = &ListNode{Val: 0}
+    currentNode := root
+
+    for i := len(array) -1; i >= 0; i-- {
+        intval, _ := strconv.Atoi(string(array[i]))
+        nextNode := &ListNode{Val: intval}
+        currentNode.Next =  nextNode
+        currentNode = nextNode
+    }
+    return root.Next;
+}
+
+func LinkedListToString(n *ListNode) string {
+    str := strconv.Itoa(n.Val)
+    for n.Next != nil{ 
+        str = str + strconv.Itoa(n.Next.Val);
+        n = n.Next;
+    } 
+    return str
+}
+
+func ReverseString(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
+
+func addLargeNumbers(num1, num2 string) string {
+
+    i := len(num1) - 1
+
+    j := len(num2) - 1
+
+    carry := 0
+
+    var result []byte
+
+
+    for i >= 0 || j >= 0 || carry != 0 {
+
+        n1 := 0
+
+        if i >= 0 {
+
+            n1 = int(num1[i] - '0')
+
+        }
+
+        n2 := 0
+
+        if j >= 0 {
+
+            n2 = int(num2[j] - '0')
+
+        }
+
+
+        current := n1 + n2 + carry
+
+        carry = current / 10
+
+        current = current % 10
+
+        result = append([]byte{'0' + byte(current)}, result...)
+
+        i--
+
+        j--
+
+    }
+
+
+    return string(result)
+
+}
